55988e037c9094c91a9667f5f935acad03d62bbb
Parker Drouillard <parker@pepcorp.ca>
2026-03-18 01:37:47 -0400

    included license locals

--- MP3D-WEBAPP/src/app.d.ts
+++ MP3D-WEBAPP/src/app.d.ts
@@ -1,4 +1,5 @@
 import type { DefaultSession } from '@auth/sveltekit';
+import type { License } from '@prisma/client';
 
 declare module '@auth/sveltekit' {
   interface Session {
@@ -15,6 +16,7 @@ declare global {
         id: string;
         email: string;
       };
+      license: License | null;
     }
   }
 }
